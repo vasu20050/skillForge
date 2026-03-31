20dc03c18f5591ea3894bd0ba37c15fe2a6b01d1
Vaibhav Sharma <vaibhavsharma@gmail.com>
2026-03-31 12:04:07 +0530

    fix: swap bcrypt for bcryptjs (pure JS, fixes Render build crash)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET || 'skillforge_secret_2026', { expiresIn: '30d' });
