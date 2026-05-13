c774f12bde672dc7f4c233352b83dbc70ea7fce7
VAIBHAV SHARMA <vaibhavsharma04814@gmail.com>
2026-05-13 23:58:02 +0530

    Update Explore.js

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -22,7 +22,6 @@ export default function Explore() {
     (selectedCategory === 'All' || bounty.category === selectedCategory) &&
     bounty.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
-
   return (
     <div className="space-y-12 pb-20 animate-in fade-in duration-1000 relative">
       
